6dded8f63ee78d36bbf74cf00e54ce2859a21c9c
ehurtado <ehurtado.open.com.co>
2020-10-19 15:57:53 -0500

    first commit

---
+++ pruebas/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sider;
+using pruebas.Util;
+
+namespace pruebas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool requirePass = true;
+            string serverHost = "\t\t\t\t<Valve alwaysBatch=\"true\" batchSize=\"50\""
+            + "\n\t\t\t\tclassName=\"com.openintl.security.valve.RedisLogValve\" expiration=\"259200\""
+            + "\n\t\t\t\tflushInterval=\"30000\" queueSize=\"50\""
+            + "\n\t\t\t\tredisHost=\"&HOST&\" redisPort=\"&PORT&\" />";
+          
+
+            Console.WriteLine(serverHost);
+            string serverHot = "hola";
+            try
+            {
+                Console.WriteLine("Inicio");
+                // Conexión con la base de datos
+                var client = new RedisClient("tmpsao445674", 6379);
+                //Console.WriteLine("1");
+                // Contraseña (si fue ingresada por el instalador)   
+                //client.Auth("qwerty");
+                Console.WriteLine(client.Ping());
+                // Selección de la base de datos
+                client.Select(0);
+
+                // Registra una llave con un valor cualquiera
+                client.Set("Installed", "RedisInstalledSFBH0708");
+
+                // Obtiene el valor de la llave
+                //Console.WriteLine(client.Get("Installed"));
+                //Console.WriteLine(client.Get("index0"));
+
+                //string comm = "mkdir /home/tomcat/new";//Console.WriteLine(Conect.EjecutarSSH("tmpsao445674", "tomcat", "XXXsrivera445674", comm));
+
+                //Console.WriteLine(Conect.test());
+
+                Console.WriteLine(Conect.validateUrl("1234567890"));
+                Console.WriteLine(Console.ReadKey());
+                Console.WriteLine("Fin");
+            }
+            catch (System.Net.Sockets.SocketException e)
+            {
+                if (e.Message.Contains("No such host is known"))
+                {
+                    Console.WriteLine("No se puede comprobar el host");
+                }
+            }
+            catch (ResponseException e)
+            {
+                if (e.Message.Contains("NOAUTH"))
+                {
+                    requirePass = false;
+                    Console.WriteLine("Necesita password");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ pruebas/Util/Conect.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tamir.SharpSsh;
+using System.Text.RegularExpressions;
+
+namespace pruebas.Util
+{
+    class Conect
+    {
+        public static string EjecutarSSH(string pServidor, string pUsuario, string pPassword, string pComando)
+        {
+            SshStream SSH = null;
+            try
+            {
+                // instancia del objeto SSHShell
+                SSH = new SshStream(pServidor, pUsuario, pPassword);
+                // ejecutar el comando
+                SSH.Write(pComando);
+
+                string ret = SSH.ReadResponse();
+                // confirmar
+                SSH.Flush();
+
+                return ret;
+            }
+            catch (Exception ex)
+            { throw ex; }
+            finally
+            {
+                //cerrar conexion SSH
+                SSH.Close();
+            }  // end try
+        } // end EjecutarSHH
+
+        public static string test()
+        {
+             
+            //Establecemos la conexión SSH
+            const string host = "tmpsao445674";
+            SshExec sshExec = new SshExec(host, "tomcat", "XXXsrivera445674");
+            sshExec.Connect();
+
+            string result = sshExec.RunCommand(@"cat /etc/redis.conf");
+
+            return result;           
+        }
+
+        public static void BajarSCP(string pServidor, string pUsuario, string pPassword, string pFileRemoto, string pFileLocal)
+        {
+            // instancia del objeto SCP
+            Scp SCP = new Scp(pServidor, pUsuario, pPassword);
+            try
+            {
+                // crear conexion SSH
+                SCP.Connect();
+                // Bajar el archivo
+                SCP.From(pFileRemoto, pFileLocal, false);
+            }
+            catch (Exception ex)
+            { throw ex; }
+            finally
+            {
+                //cerrar conexion SCP
+                SCP.Close();
+            }  // end try
+        } // end BajarSCP
+
+        public static void SubirSCP(string pServidor, string pUsuario, string pPassword, string pFileRemoto, string pFileLocal)
+        {
+            // instancia del objeto SCP
+            Scp SCP = new Scp(pServidor, pUsuario, pPassword);
+            try
+            {
+                // crear conexion SSH
+                SCP.Connect();
+                // Subir el archivo
+                SCP.To(pFileLocal, pFileRemoto, false);
+            }
+            catch (Exception ex)
+            { throw ex; }
+            finally
+            {
+                //cerrar conexion SCP
+                SCP.Close();
+            }  // end try
+        } // end SubirSCP
+
+        public static bool validateUrl(string value)
+        {
+            int i;
+            if (!int.TryParse(value, out i))
+            {
+                //no numérico
+                Console.WriteLine(i);
+                return false;
+            }
+            else
+            {
+                Console.WriteLine(i);
+                return true;
+            }
+
+            
+        }
+    }
+}
